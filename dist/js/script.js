63ddd77cc28cadfc3df8c2ed85ae9fd12c5b1fe6
Yoel Siregar <yoelsiregar05@gmail.com>
2023-11-22 20:42:05 +0700

    portfolio tailwind css

--- dist/js/script.js
+++ dist/js/script.js
@@ -19,3 +19,21 @@ hamburger.addEventListener('click', function () {
   hamburger.classList.toggle('hamburger-active');
   navMenu.classList.toggle('hidden');
 });
+
+
+function buatTautan() {
+  // Mendapatkan nilai pesan dari textarea
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("nope").value;
+  var message = document.getElementById("message").value;
+
+
+  // Membuat URL tautan dengan menambahkan nilai pesan
+  var tautan = "https://wa.me/6282276976177?text=" + 
+  "Nama :  " + encodeURIComponent(name) + ", " + "%0A" +
+  "%0ANo.hp :  " + encodeURIComponent(email) + ", " + "%0A" +
+  "%0APesan :  " + encodeURIComponent(message) + ".";
+
+  // Mengarahkan pengguna ke URL tautan
+  window.location.href = tautan;
+}
